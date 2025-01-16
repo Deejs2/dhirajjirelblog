75555b9d4f7d2b1278bf3899bbaf4b28efb556ab
Deejs2 <jireldhiraj123@gmail.com>
2025-01-16 21:05:25 +0545

    create a base layout for blog

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { BlogComponent } from './component/blog/blog.component';
+
+export const routes: Routes = [
+    {path: '', component: BlogComponent}
+];
